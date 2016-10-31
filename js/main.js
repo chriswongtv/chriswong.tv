bb8bca326250e3e5199b3a264197434db015651a
chriswongtv <chris@cogifire.com>
2016-10-30 21:57:39 -0700

    Aesthetics

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
-$(".collapse-card a i").click(function() {
-    if ($(this).css( "transform" ) == 'none' || $(this).css( "transform" ) == 'matrix(1, 0, 0, 1, 0, 0)') {
+$(".collapse-toggle").click(function() {
+    if ($(this).find(".award-toggle").css( "transform" ) == 'none' || $(this).find(".award-toggle").css( "transform" ) == 'matrix(1, 0, 0, 1, 0, 0)') {
         $(this).animate({  borderSpacing: 180 }, {
 			step: function(now,fx) {
-				$(this).css('-webkit-transform','rotate('+now+'deg)'); 
-				$(this).css('-moz-transform','rotate('+now+'deg)');
-				$(this).css('transform','rotate('+now+'deg)');
+				$(this).find(".award-toggle").css('-webkit-transform','rotate('+now+'deg)'); 
+				$(this).find(".award-toggle").css('-moz-transform','rotate('+now+'deg)');
+				$(this).find(".award-toggle").css('transform','rotate('+now+'deg)');
 			},
 			duration:'slow'
 		},'linear');
     } else {
         $(this).animate({  borderSpacing: 0 }, {
 			step: function(now,fx) {
-				$(this).css('-webkit-transform','rotate('+now+'deg)'); 
-				$(this).css('-moz-transform','rotate('+now+'deg)');
-				$(this).css('transform','rotate('+now+'deg)');
+				$(this).find(".award-toggle").css('-webkit-transform','rotate('+now+'deg)'); 
+				$(this).find(".award-toggle").css('-moz-transform','rotate('+now+'deg)');
+				$(this).find(".award-toggle").css('transform','rotate('+now+'deg)');
 			},
 			duration:'slow'
 		},'linear');
