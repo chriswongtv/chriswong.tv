839abffd182299beb8b446b88c066774f2261c09
chriswongtv <chris@cogifire.com>
2016-11-01 12:26:59 -0700

    Travel init

--- js/main.js
+++ js/main.js
@@ -19,3 +19,12 @@ $(".collapse-toggle").click(function() {
 		},'linear');
     }
 });
+
+document.getElementById('links').onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
