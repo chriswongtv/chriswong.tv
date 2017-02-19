ce6eb8171331945d58866dfa761aaf1bc0cb5567
chriswongtv <chris@cogifire.com>
2017-02-18 18:25:16 -0800

    Hero

--- js/main.js
+++ js/main.js
@@ -1,21 +1,18 @@
-$(".collapse-toggle").click(function() {
-    if ($(this).find(".award-toggle").css( "transform" ) == 'none' || $(this).find(".award-toggle").css( "transform" ) == 'matrix(1, 0, 0, 1, 0, 0)') {
-        $(this).animate({  borderSpacing: 180 }, {
-			step: function(now,fx) {
-				$(this).find(".award-toggle").css('-webkit-transform','rotate('+now+'deg)'); 
-				$(this).find(".award-toggle").css('-moz-transform','rotate('+now+'deg)');
-				$(this).find(".award-toggle").css('transform','rotate('+now+'deg)');
-			},
-			duration:'slow'
-		},'linear');
-    } else {
-        $(this).animate({  borderSpacing: 0 }, {
-			step: function(now,fx) {
-				$(this).find(".award-toggle").css('-webkit-transform','rotate('+now+'deg)'); 
-				$(this).find(".award-toggle").css('-moz-transform','rotate('+now+'deg)');
-				$(this).find(".award-toggle").css('transform','rotate('+now+'deg)');
-			},
-			duration:'slow'
-		},'linear');
-    }
-});
+var hero = new Vue({
+	el: '#hero',
+	data: {
+		topbar: false,
+	},
+	mounted: function() {
+		window.addEventListener('scroll', this.mainScroll);
+	},
+	methods: {
+		mainScroll: function() {
+			if (window.scrollY > 0 && !this.topbar) {
+				this.topbar = true;
+			} else if (window.scrollY === 0 && this.topbar) {
+				this.topbar = false;
+			}
+		},
+	}
+})
