7823a936d76e9127ee054887338cdb4db4731b70
chriswongtv <chris@cogifire.com>
2017-02-19 18:50:44 -0800

    Work

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
-var bg = new Vue({
-	el: '#background',
-	data: {
-		about: false
-	},
-	mounted: function() {
-		window.addEventListener('scroll', this.mainScroll);
-	},
-	methods: {
-		mainScroll: function() {
-			if (window.scrollY >= (window.innerHeight - window.innerHeight * .4) && !this.about) {
-				this.about = true;
-			} else if (window.scrollY < (window.innerHeight - window.innerHeight * .4) && this.about) {
-				this.about = false;
-			}
-		},
-	}
-})
+// var bg = new Vue({
+// 	el: '#background',
+// 	data: {
+// 		about: false
+// 	},
+// 	mounted: function() {
+// 		window.addEventListener('scroll', this.mainScroll);
+// 	},
+// 	methods: {
+// 		mainScroll: function() {
+// 			
+// 		},
+// 	}
+// })
 
-var hero = new Vue({
-	el: '#hero',
-	data: {
-		about: false,
-	}
+// var hero = new Vue({
+// 	el: '#hero',
+// 	data: {
+// 		about: false,
+// 	}
+// })
+
+$(document).ready(function() {
+	window.addEventListener('scroll', function() {
+		if (window.scrollY >= (window.innerHeight - window.innerHeight * .5) &&
+			window.scrollY < (window.innerHeight + window.innerHeight * .5)) {
+			$("#background").removeClass("bg-cogifire").addClass("bg-about");
+			$("#about").addClass("active");
+		} else if (window.scrollY >= (window.innerHeight + window.innerHeight * .5) &&
+					window.scrollY < (window.innerHeight * 2 + window.innerHeight * .5)) {
+			$("#background").removeClass("bg-about").removeClass("bg-hackuci").addClass("bg-cogifire");
+			$("#cogifire").addClass("active");
+		} else if (window.scrollY >= (window.innerHeight * 2 + window.innerHeight * .5)) {
+			$("#background").removeClass("bg-cogifire").addClass("bg-hackuci");
+			$("#hackuci").addClass("active");
+		} else if (window.scrollY < (window.innerHeight - window.innerHeight * .5)) {
+			$("#background").removeClass("bg-about");
+			$(".about").removeClass("active");
+		}
+	});
 })
