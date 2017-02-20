5776e90357c63fad08aca933683fdc24e4303e41
chriswongtv <chris@cogifire.com>
2017-02-20 10:19:02 -0800

    Fixed scrolling across devices

--- js/main.js
+++ js/main.js
@@ -1,66 +1,66 @@
 $(document).ready(function() {
 	window.addEventListener('scroll', function() {
 		if ($(window).width() > 991) {
-			if (window.scrollY >= (window.innerHeight - window.innerHeight * .5 - window.innerHeight * .05) &&
-				window.scrollY < (window.innerHeight + window.innerHeight * .5 - window.innerHeight * .05)) {
+			if (window.scrollY >= ($(window).height() - $(window).height() * .5 - $(window).height() * .05) &&
+				window.scrollY < ($(window).height() + $(window).height() * .5 - $(window).height() * .05)) {
 				$("#background").removeClass("bg-cogifire").addClass("bg-about");
 				$("#about").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight + window.innerHeight * .5 - window.innerHeight * .1) &&
-						window.scrollY < (window.innerHeight * 2 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() + $(window).height() * .5 - $(window).height() * .1) &&
+						window.scrollY < ($(window).height() * 2 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-about").removeClass("bg-hackuci").addClass("bg-cogifire");
 				$("#cogifire").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 2 + window.innerHeight * .5 - window.innerHeight * .1) &&
-						window.scrollY < (window.innerHeight * 3 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() * 2 + $(window).height() * .5 - $(window).height() * .1) &&
+						window.scrollY < ($(window).height() * 3 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-cogifire").removeClass("bg-coolection").addClass("bg-hackuci");
 				$("#hackuci").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 3 + window.innerHeight * .5 - window.innerHeight * .1) &&
-						window.scrollY < (window.innerHeight * 4 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() * 3 + $(window).height() * .5 - $(window).height() * .1) &&
+						window.scrollY < ($(window).height() * 4 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-hackuci").removeClass("bg-clow").addClass("bg-coolection");
 				$("#coolection").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 4 + window.innerHeight * .5 - window.innerHeight * .1) &&
-						window.scrollY < (window.innerHeight * 5 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() * 4 + $(window).height() * .5 - $(window).height() * .1) &&
+						window.scrollY < ($(window).height() * 5 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-coolection").removeClass("bg-joyfeed").addClass("bg-clow");
 				$("#clow").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 5 + window.innerHeight * .5 - window.innerHeight * .1) &&
-						window.scrollY < (window.innerHeight * 6 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() * 5 + $(window).height() * .5 - $(window).height() * .1) &&
+						window.scrollY < ($(window).height() * 6 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-clow").removeClass("bg-contact").addClass("bg-joyfeed");
 				$("#joyfeed").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 6 + window.innerHeight * .5 - window.innerHeight * .1)) {
+			} else if (window.scrollY >= ($(window).height() * 6 + $(window).height() * .5 - $(window).height() * .1)) {
 				$("#background").removeClass("bg-joyfeed").addClass("bg-contact");
 				$("#contact").addClass("active");
-			} else if (window.scrollY < (window.innerHeight - window.innerHeight * .5 - window.innerHeight * .05)) {
+			} else if (window.scrollY < ($(window).height() - $(window).height() * .5 - $(window).height() * .05)) {
 				$("#background").removeClass("bg-about");
 				$(".about").removeClass("active");
 			}
 		} else {
-			if (window.scrollY >= (window.innerHeight - window.innerHeight * .5) &&
-				window.scrollY < (window.innerHeight + window.innerHeight * .5)) {
+			if (window.scrollY >= ($(window).height() - $(window).height() * .5) &&
+				window.scrollY < ($(window).height() + $(window).height() * .5)) {
 				$("#background").removeClass("bg-cogifire").addClass("bg-about");
 				$("#about").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight + window.innerHeight * .5) &&
-						window.scrollY < (window.innerHeight * 2 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() + $(window).height() * .5) &&
+						window.scrollY < ($(window).height() * 2 + $(window).height() * .5 + $(window).height() * .15)) {
 				$("#background").removeClass("bg-about").removeClass("bg-hackuci").addClass("bg-cogifire");
 				$("#cogifire").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 2 + window.innerHeight * .5) &&
-						window.scrollY < (window.innerHeight * 3 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() * 2 + $(window).height() * .5 + $(window).height() * .15) &&
+						window.scrollY < ($(window).height() * 3 + $(window).height() * .5 + $(window).height() * .20)) {
 				$("#background").removeClass("bg-cogifire").removeClass("bg-coolection").addClass("bg-hackuci");
 				$("#hackuci").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 3 + window.innerHeight * .5) &&
-						window.scrollY < (window.innerHeight * 4 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() * 3 + $(window).height() * .5 + $(window).height() * .20) &&
+						window.scrollY < ($(window).height() * 4 + $(window).height() * .5 + $(window).height() * .25)) {
 				$("#background").removeClass("bg-hackuci").removeClass("bg-clow").addClass("bg-coolection");
 				$("#coolection").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 4 + window.innerHeight * .5) &&
-						window.scrollY < (window.innerHeight * 5 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() * 4 + $(window).height() * .5 + $(window).height() * .25) &&
+						window.scrollY < ($(window).height() * 5 + $(window).height() * .5 + $(window).height() * .35)) {
 				$("#background").removeClass("bg-coolection").removeClass("bg-joyfeed").addClass("bg-clow");
 				$("#clow").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 5 + window.innerHeight * .5) &&
-						window.scrollY < (window.innerHeight * 6 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() * 5 + $(window).height() * .5 + $(window).height() * .35) &&
+						window.scrollY < ($(window).height() * 6 + $(window).height() * .5 + $(window).height() * .45)) {
 				$("#background").removeClass("bg-clow").removeClass("bg-contact").addClass("bg-joyfeed");
 				$("#joyfeed").addClass("active");
-			} else if (window.scrollY >= (window.innerHeight * 6 + window.innerHeight * .5)) {
+			} else if (window.scrollY >= ($(window).height() * 6 + $(window).height() * .5 + $(window).height() * .45)) {
 				$("#background").removeClass("bg-joyfeed").addClass("bg-contact");
 				$("#contact").addClass("active");
-			} else if (window.scrollY < (window.innerHeight - window.innerHeight * .5)) {
+			} else if (window.scrollY < ($(window).height() - $(window).height() * .5)) {
 				$("#background").removeClass("bg-about");
 				$(".about").removeClass("active");
 			}
